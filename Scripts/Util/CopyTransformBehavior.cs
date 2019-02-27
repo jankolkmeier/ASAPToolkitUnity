42920cf3a1736d33dbd29a6630a899d1ea9b683b
Jan Kolkmeier <jankolkmeier@gmail.com>
2019-02-27 14:44:24 +0100

    Added copytransformbehavior

---
+++ Scripts/Util/CopyTransformBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransformBehavior : MonoBehaviour {
+
+	// TODO: set relative cos..
+
+	public bool copyRotation = true;
+	public bool copyPosition = true;
+	public Transform src;
+	public Transform target;
+
+	void Awake() {
+		if (target == null) target = transform;
+		if (src == null) src = transform;
+	}
+
+	void Update () {
+		Apply();
+	}
+
+	public void Apply() {
+		if (copyRotation) target.transform.rotation = src.rotation;
+		if (copyPosition) target.transform.position = src.position;
+	}
+}
