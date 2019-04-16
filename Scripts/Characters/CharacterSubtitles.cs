0f896fad3165b1849f83458c153e7aa32dde2771
Jan Kolkmeier <jankolkmeier@gmail.com>
2019-04-16 15:33:12 +0200

    added subtitle support

---
+++ Scripts/Characters/CharacterSubtitles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ASAPToolkit.Unity.Characters {
+    public class CharacterSubtitles : MonoBehaviour {
+
+        public Text text;
+
+        public void ShowSubtitles(string content) {
+            text.gameObject.SetActive(true);
+            text.text = content;
+        }
+
+        public void HideSubtitles() {
+            text.gameObject.SetActive(false);
+        }
+
+    }
+}
